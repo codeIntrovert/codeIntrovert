2c7d3fab26b10ff75785cd7ad4de45730d06641a
codeIntrovert <9131411681hasan@gmail.com>
2024-01-09 00:47:44 -0800

    json bug fix

--- update-readme.js
+++ update-readme.js
@@ -19,39 +19,27 @@ async function updateReadme() {
 
     const data = await response.json();
 
-    const quote = data.quote || "No quote available";
-    const author = data.author || "Unknown author";
+    // Check if the response is an array and contains at least one object
+    if (Array.isArray(data) && data.length > 0) {
+      const quote = data[0].quote || "No quote available";
+      const author = data[0].author || "Unknown author";
 
-    //Checking API response
-    console.log("API Response:", JSON.stringify(data, null, 2));
+      const readmePath = "README.md";
+      let readmeContent = fs.readFileSync(readmePath, "utf-8");
 
-    // Debugging: Print current working directory
-    console.log("Current working directory:", process.cwd());
+      // Update README.md content with the obtained quote and author
+      readmeContent = readmeContent.replace(
+        /<!-- QUOTE_START -->.*<!-- QUOTE_END -->/s,
+        `<!-- QUOTE_START -->\n\n"${quote}" - ${author}\n\n<!-- QUOTE_END -->`
+      );
 
-    // Debugging: Print contents of README.md
-    console.log(
-      "Contents of README.md before update:",
-      fs.readFileSync("README.md", "utf-8")
-    );
+      fs.writeFileSync(readmePath, readmeContent);
 
-    const readmePath = "README.md";
-    let readmeContent = fs.readFileSync(readmePath, "utf-8");
-
-    // Update README.md content with the obtained quote and author
-    readmeContent = readmeContent.replace(
-      /<!-- QUOTE_START -->.*<!-- QUOTE_END -->/s,
-      `<!-- QUOTE_START -->\n\n"${quote}" - ${author}\n\n<!-- QUOTE_END -->`
-    );
-
-    fs.writeFileSync(readmePath, readmeContent);
-
-    // Debugging: Print contents of README.md after update
-    console.log(
-      "Contents of README.md after update:",
-      fs.readFileSync("README.md", "utf-8")
-    );
-
-    console.log("README.md updated successfully.");
+      console.log("README.md updated successfully.");
+    } else {
+      console.error("API response is not in the expected format:", data);
+      process.exit(1);
+    }
   } catch (error) {
     console.error("Error updating README.md:", error.message);
     process.exit(1);
