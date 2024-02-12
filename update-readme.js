026c1fd19dd73df1548cda2ff3beb684c524e059
Hasan <9131411681hasan@gmail.com>
2024-02-12 22:06:05 +0530

    changed API Endpoint to zenquotes.io

--- update-readme.js
+++ update-readme.js
@@ -4,16 +4,11 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 async function updateReadme() {
-  const apiKey = process.env.API_KEY;
 
-  const apiEndpoint = "https://api.api-ninjas.com/v1/quotes?category=happiness";
+  const apiEndpoint = "https://zenquotes.io/api/random";
 
   try {
-    const response = await fetch(apiEndpoint, {
-      headers: {
-        "X-Api-Key": apiKey,
-      },
-    });
+    const response = await fetch(apiEndpoint);
 
     if (!response.ok) {
       throw new Error(`Failed to fetch data. Status: ${response.status}`);
@@ -21,19 +16,21 @@ async function updateReadme() {
 
     const data = await response.json();
     // Add this code after the data is fetched
-    console.log("API Response:", JSON.stringify(data, null, 2));
+   // console.log("API Response:", JSON.stringify(data, null, 2)); used for debugging 
 
     if (Array.isArray(data) && data.length > 0) {
-      const quote = data[0].quote || "No quote available";
-      const author = data[0].author || "Unknown author";
+      const quote = data[0].q || "No quote available";
+      const author = data[0].a || "Unknown author";
 
+      // console.log(`${quote} by ${author}`); used for debugging
+      
       const readmePath = "README.md";
       let readmeContent = fs.readFileSync(readmePath, "utf-8");
 
       // Update README.md content with the obtained quote and author
       readmeContent = readmeContent.replace(
-        /<!-- QUOTE_START -->.*<!-- QUOTE_END -->/s,
-        `<!-- QUOTE_START -->\n\n"${quote}" - ${author}\n\n<!-- QUOTE_END -->`
+        /# Quote Of the day.*# Powered By zenquotes.io/s,
+        `# Quote Of the day\n\n"${quote}" - ${author}\n\n# Powered By zenquotes.io`
       );
 
       fs.writeFileSync(readmePath, readmeContent);
@@ -50,7 +47,7 @@ async function updateReadme() {
       await exec("git config --local user.name 'GitHub Action'");
       await exec("git add README.md");
       await exec('git commit -m "Update README with new quote"');
-      await exec("git push");
+      await exec("git push"); 
     } else {
       console.error("API response is not in the expected format:", data);
       process.exit(1);
